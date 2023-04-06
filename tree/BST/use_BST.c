1e9dfed2a393c8c45bff0f02ae1a0a2e42f7a473
Steven Chou <stevenchou499@gmail.com>
2023-04-06 23:37:51 +0800

    Add test cases

---
+++ tree/BST/use_BST.c
@@ -0,0 +1,31 @@
+#include "BST.h"
+
+int main(void)
+{
+    TN_t *root = NULL;
+    root = InitTree();
+
+    root = InsertNode(root, 10);
+    root = InsertNode(root, 6);
+    root = InsertNode(root, 12);
+    root = InsertNode(root, 3);
+    root = InsertNode(root, 8);
+    root = InsertNode(root, 11);
+    root = InsertNode(root, 15);
+    root = InsertNode(root, 5);
+    root = InsertNode(root, 7);
+    root = InsertNode(root, 2);
+    root = InsertNode(root, 1);
+
+    PreOrderTraverse(root);
+    printf("\n");
+    InOrderTraverse(root);
+    printf("\n");
+    PostOrderTraverse(root);
+    printf("\n");
+
+    printf("The depth of the binary tree is %u\n", TreeDepth(root));
+
+    DestroyTree(root);
+    return 0;
+}
